2e288abb82685e54396aef64741b004f52726360
g1franc <guillaume.francois55@gmail.com>
2016-08-21 17:21:05 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 build_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-(cd "${build_dir}" && exec docker build -t saiban/space-engineers .)
+(cd "${build_dir}" && exec docker build -t se-server/space-engineers .)
